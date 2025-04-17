2dcd73202c83419512a065ca2b584fd9cec7854a
phucnguyenAtWork <ITITWE21082@student.hcmiu.edu.vn>
2025-04-17 15:46:47 +0700

    Creating websocket with post,get method connecting between users

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,11 +3,24 @@ package main
 import (
 	"log"
 	"server/server/db"
+	"server/server/internal/user"
+	"server/server/internal/ws"
+	"server/server/router"
 )
 
 func main() {
-	_, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
 	if err != nil {
 		log.Fatalf("Error connecting to database:%s", err)
 	}
+	userRep := user.NewRepository(dbConn.GetDB())
+	userSvc := user.NewService(userRep)
+	userHandler := user.NewHandler(userSvc)
+
+	hub := ws.NewHub()
+	wsHandler := ws.NewHandler(hub)
+	go hub.Run()
+	router.InitRouter(userHandler, wsHandler)
+	router.Start("0.0.0.0:8080")
+
 }
